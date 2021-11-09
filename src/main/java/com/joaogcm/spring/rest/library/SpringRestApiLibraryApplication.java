34917ba5df0cebc591a82cf7ef4b4e8cebeca677
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2021-11-09 16:45:52 -0300

    Primeiro Commit Library

---
+++ src/main/java/com/joaogcm/spring/rest/library/SpringRestApiLibraryApplication.java
@@ -0,0 +1,11 @@
+package com.joaogcm.spring.rest.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiLibraryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiLibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/joaogcm/spring/rest/library/SpringRestApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaogcm.spring.rest.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
